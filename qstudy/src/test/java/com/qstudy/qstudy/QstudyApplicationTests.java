41d679eca8f2f1c8b540213998e77bc7b93e7d11
iam6ukk <kyk001020@naver.com>
2023-12-22 23:21:02 +0900

    commit

---
+++ qstudy/src/test/java/com/qstudy/qstudy/QstudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qstudy.qstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QstudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
