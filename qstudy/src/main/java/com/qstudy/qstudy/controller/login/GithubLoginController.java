4604c15316f06ba902883f890e7a83d50f4c1e69
hdev1004 <h.dev@kakao.com>
2024-01-10 23:02:03 +0900

    commit

--- qstudy/src/main/java/com/qstudy/qstudy/controller/login/GithubLoginController.java
+++ qstudy/src/main/java/com/qstudy/qstudy/controller/login/GithubLoginController.java
@@ -1,5 +1,25 @@
 package com.qstudy.qstudy.controller.login;
 
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qstudy.qstudy.service.login.GithubLoginService;
+
+@RestController
+@CrossOrigin
 public class GithubLoginController {
+	private final GithubLoginService githubLoginService = new GithubLoginService();
+	
+	@GetMapping(value = "/login/github")
+	 public ResponseEntity<?> LoginGithub(){
+       HashMap<String, Object> result = new HashMap<>();
+
+       return new ResponseEntity<>(result, HttpStatus.OK);
+   }
 
 }
--- qstudy/src/main/java/com/qstudy/qstudy/controller/login/GoogleLoginController.java
+++ qstudy/src/main/java/com/qstudy/qstudy/controller/login/GoogleLoginController.java
@@ -1,5 +1,26 @@
 package com.qstudy.qstudy.controller.login;
 
-public class GoogleLoginController {
+import java.util.HashMap;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qstudy.qstudy.service.login.GoogleLoginService;
+
+@RestController
+@CrossOrigin
+public class GoogleLoginController {
+	private final GoogleLoginService googleLoginService = new GoogleLoginService();
+	
+	@PostMapping(value = "/login/google")
+	 public ResponseEntity<?> LoginGoogle(@RequestBody Object requestBody){
+       HashMap<String, Object> result = new HashMap<>();
+		System.out.println(requestBody);
+
+      return new ResponseEntity<>(result, HttpStatus.OK);
+  }
 }
