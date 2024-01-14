5a580018820466da9eac3f1fb9dcdabcdd1cd123
iam6ukk <kyk001020@naver.com>
2024-01-14 22:53:35 +0900

    id 추가

--- qstudy/src/main/java/com/qstudy/qstudy/mapper/login/GoogleLoginMapper.java
+++ qstudy/src/main/java/com/qstudy/qstudy/mapper/login/GoogleLoginMapper.java
@@ -1,12 +1,11 @@
 package com.qstudy.qstudy.mapper.login;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Repository;
 
 import com.qstudy.qstudy.dto.login.User;
 
-@Repository
 @Mapper
 public interface GoogleLoginMapper {
 	void saveGoogle(User user);
 }
+ 
