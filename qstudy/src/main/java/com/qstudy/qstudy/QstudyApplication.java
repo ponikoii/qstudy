7cd77a894a032ad62a28e84bc9c51e91d68c2463
hdev1004 <h.dev@kakao.com>
2024-03-11 23:58:21 +0900

    빌딩

--- qstudy/src/main/java/com/qstudy/qstudy/QstudyApplication.java
+++ qstudy/src/main/java/com/qstudy/qstudy/QstudyApplication.java
@@ -6,10 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 @SpringBootApplication
 @RestController
 public class QstudyApplication {
+	
     public static void main(String[] args) {
       SpringApplication.run(QstudyApplication.class, args);
     }
+
 }
